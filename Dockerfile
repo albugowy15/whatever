83851e182a8adc80731c44757bca9d164b34ff09
albugowy15 <kholidbughowi@gmail.com>
2023-09-09 21:53:01 +0700

    feat: add random forest

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# https://hub.docker.com/_/python
+FROM python:3.10-slim-bullseye
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+COPY . /code
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 import joblib
 import os
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
 
@@ -12,27 +14,38 @@ rf_model = joblib.load(rf_model_path)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class InputData(BaseModel):
     heart_rate: int
     systolic_blood_pressure: int
     diastolic_blood_pressure: int
-    body_temp: float
+    body_temp_c: float
 
-label_mapping = {
-    "Relaxed": 0,
-    "Calm": 1,
-    "Tense": 2,
-    "Stressed": 3
-}
+labels = ["Relaxed", "Calm", "Tense", "Stressed"];
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/predict/rf/")
+@app.post("/predict/rf")
 async def predict_stress_level(input_data: InputData):
-    features = [input_data.heart_rate, input_data.systolic_blood_pressure, input_data.diastolic_blood_pressure, input_data.body_temp]
-    prediction = rf_model.predict([features])
-    print(prediction)
-    return {"stress_level_index": "none", "stress_level_label": "none"}
+    incoming_data = pd.DataFrame({
+        'heart_rate': [input_data.heart_rate],
+        'systolic_blood_pressure': [input_data.systolic_blood_pressure],
+        'diastolic_blood_pressure': [input_data.diastolic_blood_pressure],
+        'body_temp_c': [input_data.body_temp_c]
+    })
+    prediction = rf_model.predict(incoming_data)
+    print(prediction[0])
+    print(type(prediction[0]))
+    return {"stress_level_index": int(prediction[0]), "stress_level_label": labels[int(prediction[0])]}
